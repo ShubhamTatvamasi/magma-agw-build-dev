5d6a0a37b9467562581aef8f15de4496bc752c8d
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-11-23 13:40:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,15 @@ set -ex
 
 # Install prerequisites
 pip3 install ansible fabric3 jsonpickle requests PyYAML
-vagrant plugin install vagrant-vbguest
+vagrant plugin install vagrant-vbguest vagrant-disksize vagrant-vbguest vagrant-mutate
 
 # Cloning magma repo:
 git clone https://github.com/magma/magma.git --depth 1
+
+# Open up network interfaces for VM
+sudo mkdir -p /etc/vbox/
+sudo touch /etc/vbox/networks.conf
+sudo sh -c "echo '* 192.168.0.0/16' > /etc/vbox/networks.conf"
 
 # start building magma
 cd magma/lte/gateway
